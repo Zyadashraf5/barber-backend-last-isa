4efde0b0705a2cba52f4dd075b3300dda6c8377f
samehmaher <82654769+Zyadashraf5@users.noreply.github.com>
2024-12-11 21:53:20 +0200

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,72 +1,45 @@
 const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
-const { PrismaClient } =require( '@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 // Utility function to parse pagination parameters
-const getPagination = (query) => {
-    const page = parseInt(query.page, 12) || 1; // Default to page 1
-    const limit = parseInt(query.limit, 12) || 12; // Default to 12 items per page
-    const skip = (page - 1) * limit; // Calculate how many items to skip
-    return { page, limit, skip };
-};
 
 // Admin: Get all barbers with pagination
 exports.adminGetAllBarbers = catchAsync(async (req, res, next) => {
-    try {
-        const barbers = await prisma.user.findMany({
-            where: {
-                role: 'Barber', // Filter users where role is 'barber'
-            },
-        });
+    const barbers = await prisma.user.findMany({
+        where: {
+            role: "Barber", // Filter users where role is 'barber'
+        },
+    });
 
-        res.status(200).json({
-            status: 'success',
-            data: barbers,
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            status: 'error',
-            message: error.message,
-        });
-    }
+    res.status(200).json({
+        status: "success",
+        data: barbers,
+    });
 });
 
 // Admin: Get all users with pagination
 exports.adminGetAllUsers = catchAsync(async (req, res, next) => {
-    const { page, limit, skip } = getPagination(req.query);
-
     const users = await prisma.user.findMany({
         where: {
-            role: 'User', // Filter users where role is 'barber'
+            role: "User", // Filter users where role is 'barber'
         },
-        skip,
-        take: limit,
         include: {
             // bookings: true,
             // barberReviews: true,
         },
     });
 
-    const totalCount = await prisma.user.count();
-
     res.status(200).json({
-        status: 'success',
-        page,
-        totalPages: Math.ceil(totalCount / limit),
-        totalCount,
+        status: "success",
         data: users,
     });
 });
 
 // Admin: Get all bookings with pagination
 exports.adminGetAllBookings = catchAsync(async (req, res, next) => {
-    const { page, limit, skip } = getPagination(req.query);
-
     const bookings = await prisma.booking.findMany({
-        skip: skip,
-        take: limit,
         include: {
             user: true, // Include user who made the booking
             barberStore: true, // Include barber store details
@@ -77,12 +50,12 @@ exports.adminGetAllBookings = catchAsync(async (req, res, next) => {
             },
         },
         orderBy: {
-            Date: 'asc', // Order bookings by date (ascending)
+            Date: "asc", // Order bookings by date (ascending)
         },
     });
 
     res.status(200).json({
-        status: 'success',
+        status: "success",
         results: bookings.length,
         data: bookings,
     });
@@ -91,16 +64,20 @@ exports.adminGetAllBookings = catchAsync(async (req, res, next) => {
 // Admin: ban user
 exports.banUser = catchAsync(async (req, res, next) => {
     const { userId } = req.params;
-
+    let user = await prisma.user.findUnique({
+        where: {
+            id: +userId,
+        },
+    });
     // Update the user's `ban` column to true
-    const user = await prisma.user.update({
+    user = await prisma.user.update({
         where: { id: +userId },
-        data: { ban: true },
+        data: { ban: !user.ban },
     });
 
     // Send a success response
     res.status(200).json({
-        status: 'success',
+        status: "success",
         message: `User with ID ${userId} has been banned.`,
         data: user,
     });
@@ -117,7 +94,7 @@ exports.unBanUser = catchAsync(async (req, res, next) => {
 
     // Send a success response
     res.status(200).json({
-        status: 'success',
+        status: "success",
         message: `User with ID ${userId} has been Unbanned.`,
         data: user,
     });
